b8d297b37b5aec0c05a542e0b6553147d0261991
ikeyu0806 <ikeyu0806@gmail.com>
2025-04-19 18:15:49 +0900

    add fetch tool, clean_file tool

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import {
 } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import { z } from 'zod'
+import { promises as fs } from 'fs'
+import path from 'path'
+import os from 'os'
+import trash from 'trash'
 
 // Create an MCP server
 const server = new McpServer({
@@ -16,6 +20,50 @@ const server = new McpServer({
 server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
   content: [{ type: 'text', text: String(a + b) }],
 }))
+
+server.tool(
+  'fetch_it_news',
+  'ITニュースを収集する',
+  { target: z.string() },
+  async ({ target }) => {
+    const hotentryData = await fetch('https://b.hatena.ne.jp/hotentry/it').then(
+      (res) => res.text(),
+    )
+
+    return {
+      content: [
+        {
+          type: 'text',
+          text: hotentryData,
+        },
+      ],
+    }
+  },
+)
+
+server.tool(
+  'clean_files',
+  'ローカルファイルを削除する',
+  { target: z.string() },
+  async ({ target }) => {
+    const desktopPath = path.join(os.homedir(), 'Desktop')
+
+    const files = await fs.readdir(desktopPath)
+
+    const fullPaths = files.map((file) => path.join(desktopPath, file))
+
+    trash(fullPaths)
+
+    return {
+      content: [
+        {
+          type: 'text',
+          text: '削除しました',
+        },
+      ],
+    }
+  },
+)
 
 // SDKのREADMEにあったサンプル
 // Add a dynamic greeting resource
